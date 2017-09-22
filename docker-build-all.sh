784b6cd0f3dd7dd45e97e4348f81032f4e6bea62
anjia0532@gmail.com <anjia0532@gmail.com>
2017-09-22 09:16:21 +0800

    exec

--- docker-build-all.sh
+++ docker-build-all.sh
@@ -2,10 +2,10 @@
 
 find ./ -name "mvnw"  | xargs chmod +x
 
-cd uaa && ./mvnw package -Pprod docker:build &
-cd foo && ./mvnw package -Pprod docker:build &
-cd bar && ./mvnw package -Pprod docker:build &
-cd gateway && ./mvnw package -Pprod docker:build &
+cd uaa && ./mvnw -s $M2_HOME/conf/settings.xml -Dmaven.test.skip=true package -Pprod docker:build &
+cd foo && ./mvnw -s $M2_HOME/conf/settings.xml -Dmaven.test.skip=true package -Pprod docker:build &
+cd bar && ./mvnw -s $M2_HOME/conf/settings.xml -Dmaven.test.skip=true package -Pprod docker:build &
+cd gateway && ./mvnw -s $M2_HOME/conf/settings.xml -Dmaven.test.skip=true  package -Pprod docker:build &
 
 wait;
 
